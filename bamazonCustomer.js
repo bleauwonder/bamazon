df91a11187ba55ada6c38f73b32ecda9dbc9573f
bleauwonder <elyzableau.om@gmail.com>
2019-08-15 14:51:45 -0400

    hid password with dotenv

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
-  password: "glideThewave6!",
+  password: process.env.DBPASS,
   database: "bamazonDB"
 });
 
